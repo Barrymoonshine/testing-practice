1caeeb62e4deaac7f296cec511e73e3e6d91a1e7
Nick Sheward <nickshwrd@gmail.com>
2023-04-21 11:22:04 +0100

    Created calculator function and test

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const capitalize = require('./functions/capitalize');
 const reverseString = require('./functions/reverseString');
+const calculator = require('./functions/calculator');
 
 test('the fist letter of a string is capitalized', () => {
   expect(capitalize('hello')).toBe('Hello');
@@ -7,4 +8,11 @@ test('the fist letter of a string is capitalized', () => {
 
 test('the string has been reversed', () => {
   expect(reverseString('hello')).toBe('olleh');
+});
+
+test('two numbers are being correctly computed', () => {
+  expect(calculator.add(10, 2)).toBe(12);
+  expect(calculator.subtract(10, 2)).toBe(8);
+  expect(calculator.multiply(10, 2)).toBe(20);
+  expect(calculator.divide(10, 2)).toBe(5);
 });
